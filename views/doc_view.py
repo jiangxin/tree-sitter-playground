58f2cf5f2363b2e394f190620f4358e95aab9282
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2025-02-28 08:39:07 +0800

    Keep doc_view cursor location during edit
    提示词：
    
    > 在执行 setHTML 之后 doc_edit 中光标位置变动，使用 setPosition
    > 也没有改变光标位置。
    
    模型回复：
    
    > 在 Qt 中，setHtml() 操作会重置文本编辑器的状态，包括光标位置。我们
    > 需要在设置 HTML 后正确地恢复光标位置。我建议在 DocView 类中添加光标
    > 位置的保存和恢复逻辑
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- views/doc_view.py
+++ views/doc_view.py
@@ -6,3 +6,17 @@ class DocView(QTextEdit):
         super().__init__()
         self.setAcceptRichText(True)
         self.setLineWrapMode(QTextEdit.NoWrap)
+
+    def setHtml(self, html):
+        # 保存当前光标位置
+        self._cursor_position = self.textCursor().position()
+
+        # 设置 HTML
+        super().setHtml(html)
+
+        # 恢复光标位置
+        cursor = self.textCursor()
+        # 确保位置不超过文本长度
+        position = min(self._cursor_position, len(self.toPlainText()))
+        cursor.setPosition(position)
+        self.setTextCursor(cursor)
